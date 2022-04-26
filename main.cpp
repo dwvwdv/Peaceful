1b4d8715585e1d6b94150167255ffa0c6cd61e3c
dwvwdv <dwvwdv@protonmail.com>
2022-04-26 19:06:33 +0800

    QSS載入設定完成、窗口透明化處理、icon增加

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "widget.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile qss(R"(:/DarkGreen.qss)");
+    qss.open(QFile::ReadOnly);
+    if(qss.isOpen()){
+        qApp->setStyleSheet(qss.readAll());
+        qss.close();
+    }
+
     Widget w;
     w.show();
-
     return a.exec();
 }
--- widget.cpp
+++ widget.cpp
@@ -6,6 +6,12 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this->setWindowIcon(QIcon(":/icon.ico"));       //Icon設定
+    this->setAttribute(Qt::WA_TranslucentBackground);//背景透明化
+    this->setWindowFlags(Qt::FramelessWindowHint);   //無邊窗口
+//    this->setFixedSize(300,270);                           //固定窗口大小
+    this->setWindowTitle("Peaceful");	 //視窗名稱設定
+
     player = new QMediaPlayer;
     player->setMedia(QUrl("qrc:/one.mp3"));
     connect(ui->ControlList,QListWidget::doubleClicked,this,Widget::slotControl);
